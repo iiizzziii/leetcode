0c64f14caaa8cb01d73510efef632fc8a820a507
Jakub Mokoš <mokos.jakub@gmail.com>
2022-07-09 11:35:33 +0200

    *lst

--- PrintElements.py
+++ PrintElements.py
@@ -1,8 +1,9 @@
 def zzz(lst : int or str):
-    output = ""
-    for l in lst:
-        output += str(l) + " "
-    print(output)
+    # output = ""
+    # for l in lst:
+    #     output += str(l) + " "
+    # print(output)
+    print(*lst)
 
 zzz([3,4,5,6])
 zzz(["a","b","c"])
